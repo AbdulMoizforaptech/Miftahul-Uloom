fcae33a8d881a7fa209c28db6c051b4d501c8129
Abdul Moiz Hassan <abdulmoizforaptech@gmail.com>
2024-05-26 21:49:10 +0500

    Madarsah Miftahul Uloom

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>مدرسہ مفتاح العلوم</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="icon" href="data/images/Falah Logo png.png">
+</head>
+<body class="body-home">
+    <div class="black-fill"> <br><br>
+        <div class="container">
+
+        <nav class="navbar navbar-expand-lg bg-light" id="homeNav">
+            <div class="container-fluid">
+                <a class="navbar-brand"><img class="nav-logo" src="data/images/Falah Logo png.png"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">   
+                    <li class="nav-item dropdown">
+                            <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                لاگ ان
+                            </a>
+                            <ul class="dropdown-menu text-center">
+                                <li><a class="dropdown-item" href="#">ایڈمن</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">اساتذہ</a></li>
+                                <li><a class="dropdown-item" href="#">والدین</a></li>
+                            </ul>
+                        </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">رابطہ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">ماہنامہ فلاح دارین</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            شعبہ جات
+                        </a>
+                        <ul class="dropdown-menu text-center">
+                            <li><a class="dropdown-item" href="#">شعبہ حفظ و ناظرہ و قاعدہ</a></li>
+                            <li><a class="dropdown-item" href="#">شعبہ اسکول</a></li>
+                            <li><a class="dropdown-item" href="#">شعبہ تخصص</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">نظام تعلیم</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">سرورق</a>
+                    </li>
+                    
+                </ul>
+                </div>
+            </div>
+        </nav><br><br>
+
+
+        <div class="d-flex p-2">
+        <div class="card mb-5" style="width: 60%;">
+                <img src="data/images/masjid copy.jpg" class="card-img-top" alt="Madarsa Picture">
+                <div class="card-body">
+                    <h2 class="card-title text-center">مدرسہ مفتاح العلوم</h2>
+                    <p class="card-text">مدرسہ مفتاح العلوم ایک دینی ادارہ ہے جو جامع مسجد اسلامیہ نارتھ ناظم آباد بلاک این میں واقع ہے۔ اس مسجد کی بنیاد مفتی ولی حسن ٹونکی رحمہ اللہ نے رکھی تھی۔</p>
+                </div>
+        </div> <!-- card -->
+        </div>
+        
+
+        </div> <!-- container -->
+
+    </div>  <!-- black-fill -->
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
